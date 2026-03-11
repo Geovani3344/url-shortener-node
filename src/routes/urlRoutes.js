e2632e307c60d62d246b4b5b5dfd710902616681
Geovani Rodrigues <geovanni3344@outlook.com>
2026-03-11 00:15:34 -0300

    Fiz ajuste de sintaxe e lógica da URL personalizada, correção de escopo e finalização da rota shorten e refatoração da lógica de apelidos e tratamento de erros.

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -10,60 +10,82 @@ const validUrl = require('valid-url');
 
 router.post('/shorten', async (req, res) => {
     //Pega o link que veio no body da req
-    const url = req.body.url;
+    const {url, customUrl} = req.body;
+    
     try {
         // Valida se o que chegou for realmente um link
         if (!validUrl.isUri(url)) {
-            res.render('index',
-                {
-                    linkGerado: null,
-                    error: 'URL inválida!'
-                }); // aqui a gente renderiza a view e passa o erro pra ela mostrar pro user
+            return res.render('index', {
+                linkGerado: null,
+                error: 'URL inválida!'
+            }); // aqui a gente renderiza a view e passa o erro pra ela mostrar pro user
+        }
+        
+        let shortUrl;
 
-            return; // Para a execucao aqui se n funcionar
-        };
+        // Se o user enviou uma url personalizada, verifica se ele já existe no banco
+        if (customUrl) {
+            const customUrlExists = await Url.findOne({ shortId: customUrl });
+
+            if (customUrlExists) {
+                return res.render('index', {
+                    linkGerado: null,
+                    error: ' Está URL já está em uso. Por favor escolha outra.'
+                });
+            }
+            
+            // Se a URL personalizada for válida e não existir, cria o link com ela
+            shortUrl = customUrl;
+            
+            const newUrl = new Url({
+                originalUrl: url,
+                shortId: shortUrl
+            });
+
+            await newUrl.save(); 
+            
+            return res.render('index', {
+                linkGerado: `https://gr-u.onrender.com/${shortUrl}`,
+                error: null
+            }); // aqui a gente renderiza a view e passa o link curto pra ela mostrar pro user 
+        }
 
         // Verifica se a URL já existe no banco, para n criar outra igual
         const urlExists = await Url.findOne({ originalUrl: url });
 
         //se existir nao cria uma nova e devolve a que ja tem
         if (urlExists) {
-            return res.render('index',
-                {
-                    linkGerado: `https://gr-u.onrender.com/${urlExists.shortId}`,
-                    error: null
-                }); // aqui a gente renderiza a view e passa o link curto pra ela mostrar pro user
-
+            return res.render('index', {
+                linkGerado: `https://gr-u.onrender.com/${urlExists.shortId}`,
+                error: 'Este link já foi encurtado'
+            }); // aqui a gente renderiza a view e passa o link curto pra ela mostrar pro user
         }
 
-        //  Gera o ID aleatorio pro encurtador
-        const shortUrl = shortid.generate();
+        // Gera o ID aleatorio pro encurtador
+        shortUrl = shortid.generate();
 
         // Prepara o objeto com o model do db
         const newUrl = new Url({
             originalUrl: url,
-            shortId: shortUrl
+            shortId: shortUrl 
         });
 
         // Salva no banco e avisa se funcionou ou n
         await newUrl.save(); // usamos o await pq é uma funcao assincrona, entao ele espera salvar pra depois mandar a resposta
-        res.render('index',
-            {
-                linkGerado: `https://gr-u.onrender.com/${newUrl.shortId}`,
-                error: null
-            }); // aqui a gente renderiza a view e passa o link curto pra ela mostrar pro user 
-    }
+        
+        res.render('index', {
+            linkGerado: `https://gr-u.onrender.com/${shortUrl}`,
+            error: null
+        }); // aqui a gente renderiza a view e passa o link curto pra ela mostrar pro user
 
-    catch (err) {
+    } catch (err) {
         // se der erro, mostra no console e avisa o user
         console.error(err);
-        res.render('index',
-            {
-                linkGerado: null,
-                error: 'Ops, algo deu errado. Tente novamente.'
-            }); // exibe a view com a mensagem de erro
+        res.render('index', {
+            linkGerado: null,
+            error: 'Ops, algo deu errado. Tente novamente.'
+        }); // exibe a view com a mensagem de erro
     }
-
 });
 
 // ROTA PRA RENDERIZAR A PAGINA INICIAL COM O FORMULARIO
